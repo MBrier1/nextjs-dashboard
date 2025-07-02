d122148f417149c986381395df51dcbec2d647e3
MBrier1 <brier.michael.17@gmail.com>
2025-07-01 22:41:17 -0400

    update2

--- src/app/ui/login-form.jsx
+++ src/app/ui/login-form.jsx
@@ -1,9 +1,9 @@
 'use client';
 import { AtSymbolIcon, KeyIcon, ExclamationCircleIcon } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from '@/app/ui/button';
+import { Button } from '../../ui/button';
 import { useFormState, useFormStatus } from 'react-dom';
-import { authenticate } from '@/app/lib/actions';
+import { authenticate } from '../../lib/actions';
 
 export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
